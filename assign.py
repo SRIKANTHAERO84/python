001cb424de8699fbe2e1eda795906eab5a1c7f5c
SRIKANTHAERO84 <srikanthaero84@gmail.com>
2019-10-30 08:54:30 +0530

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 21:22:10 2019
+
+@author: Dell
+"""
+m_status=input(str("Enter marrital status [M/U]:"))
+gender=input(str("Enter Gender [M/F]: "))
+AGE=input(int("Enter AGE:"))
+if m_status= M :
+    print("insured")
